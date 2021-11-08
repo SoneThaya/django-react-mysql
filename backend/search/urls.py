1b21619c682d9bb751232cdf6d4dddf6a1725c9e
sone <sthaya@gmail.com>
2021-11-07 17:33:30 -0800

    setup backend route to sort, filter and seartch

--- backend/search/urls.py
+++ backend/search/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ProductFrontendAPIView
+from .views import ProductFrontendAPIView, ProductBackendAPIView
 
 
 urlpatterns = [
     path('products/frontend/', ProductFrontendAPIView.as_view()),
+    path('products/backend/', ProductBackendAPIView.as_view()),
 ]
